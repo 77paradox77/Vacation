3d44a64c89a95358f2ac2f2e16bf4a04763aae67
Reed Lambier <rlambier46@gmail.com>
2016-03-25 13:38:41 -0700

    It will add working jQuery to all three dream vacations. Switched machines

--- scripts.js
+++ scripts.js
@@ -1,38 +1,39 @@
 $(document).ready(function() {
 
-  if ($("#q1").val() === "0" && $($("#q2").val() === "0")
 
 
-  // $("form").submit(function(event) {
-  //   if ($("#q1").val() === "0" && $("#q2").val() === "0")  {
-  //     alert("heyo")
-  //   }
-  //   else if ($("#q1").val() === "1" && $("#q2").val() === "1")  {
-  //     alert("12");
-  //   }
-  //   else if ($("#q1").val() === "1" && $("#q3").val() === "1")  {
-  //     alert("13");
-  //   }
-  //   else if ($("#q1").val() === "0" && $("#q3").val() === "1")  {
-  //     alert("21");
-  //   }
-  //   else if ($("#q1").val() === "0" && $("#q3").val() === "0")  {
-  //     alert("22");
-  //   }
-  //   else if ($("#q2").val() === "0" && $("#q3").val() === "0")  {
-  //     alert("23");
-  //   }
-  //   else if ($("#q2").val() === "1" && $("#q3").val() === "1")  {
-  //     alert("31");
-  //   }
-  //   else if ($("#q2").val() === "1" && $("#q3").val() === "0")  {
-  //     alert("32");
-  //   }
-  //   else if ($("#q2").val() === "0" && $("#q3").val() === "1")  {
-  //     alert("33");
-  //   }
-  //   else {
-  //     $("#vacations").hide();
-  //   }
-  // });
+$("form").submit(function(event) {
+  if ($("#q1").val() === "1" && $("#q2").val() === "1")  {
+    $(".vac3").show();
+  }
+  else if ($("#q1").val() === "1" && $("#q2").val() === "1")  {
+    $(".vac3").show();
+  }
+  else if ($("#q1").val() === "1" && $("#q3").val() === "1")  {
+    $(".vac3").show();
+  }
+  else if ($("#q1").val() === "0" && $("#q3").val() === "1")  {
+    $(".vac2").show();
+  }
+  else if ($("#q1").val() === "0" && $("#q3").val() === "0")  {
+    $(".vac1").show();
+  }
+  else if ($("#q2").val() === "0" && $("#q3").val() === "0")  {
+    $(".vac2").show();
+  }
+  else if ($("#q2").val() === "1" && $("#q3").val() === "1")  {
+    $(".vac1").show();
+  }
+  else if ($("#q2").val() === "1" && $("#q3").val() === "0")  {
+    $(".vac3").show();
+  }
+  else if ($("#q2").val() === "0" && $("#q3").val() === "1")  {
+    $("vac1").show();
+  }
+  else {
+    $(".vacations").hide();
+  }
+
+  event.preventDefault();
+});
 });
